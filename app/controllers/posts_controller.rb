106a6c18fa70bcfca1849242aa92c168332ff7f4
YuKitAs <moncher.merci@gmail.com>
2018-04-11 18:12:07 +0200

    Import .rubocop.yml and refactor ruby files according to new rules

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    render json: @posts
+    render(json: @posts)
   end
 
   def show
     @post = Post.find(params[:id])
-    render json: @post
+    render(json: @post)
   end
 
   def create
--- db/migrate/20180323131037_create_posts.rb
+++ db/migrate/20180323131037_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.text :title
-      t.text :content
+      t.text(:title)
+      t.text(:content)
 
       t.timestamps
     end
