37512f987e41a9bd576654cd51f0cfacf1e7423d
YuKitAs <moncher.merci@gmail.com>
2018-04-18 23:59:16 +0200

    Extract exception handling to a new concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
   include TokenAuthenticatable
   include AdminAuthorizable
-
-  rescue_from ActiveRecord::RecordNotFound, with: ->{ render json: { error: "Not Found" }, status: 404 }
-  rescue_from ActiveRecord::RecordInvalid, with: ->{ render json: { error: "Bad Request" }, statsu: 400 }
+  include RecordAccessible
 end
---
+++ app/controllers/concerns/record_accessible.rb
@@ -0,0 +1,8 @@
+module RecordAccessible
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound, with: ->{ render json: { error: "Not Found" }, status: 404 }
+    rescue_from ActiveRecord::RecordInvalid, with: ->{ render json: { error: "Bad Request" }, statsu: 400 }
+  end
+end
