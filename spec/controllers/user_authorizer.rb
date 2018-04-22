2d9f8ffad6fbe85d2559aa814ef17284569ec307
YuKitAs <moncher.merci@gmail.com>
2018-04-23 01:41:02 +0200

    Small refactoring for UserAuthorizer

--- spec/controllers/user_authorizer.rb
+++ spec/controllers/user_authorizer.rb
@@ -1,13 +1,15 @@
 class UserAuthorizer
-  def self.current_user(access_level)
-    current_user = User.new(email: "test@email.com", password: "nopassword")
+  class << self
+    def current_user(access_level)
+      current_user = User.new(email: "test@email.com", password: "nopassword")
 
-    current_user.instance_exec(access_level) do |level|
-      define_singleton_method(:admin?) do
-        return level == :admin
+      current_user.instance_exec(access_level) do |level|
+        define_singleton_method(:admin?) do
+          return level == :admin
+        end
       end
-    end
 
-    return current_user
+      return current_user
+    end
   end
 end
